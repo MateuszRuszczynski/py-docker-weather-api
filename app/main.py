f0c0d1eb30342179e0525b17aecf1445b7e34fef
Mateusz <mateusz.ruszczynski@gmail.com>
2026-04-27 13:20:40 +0200

    feat: create get_weather function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import requests
+import os
+
+API_KEY = os.environ.get("API_KEY")
+
+
+def get_weather() -> dict:
+    response = requests.get(
+        "http://api.weatherapi.com/v1/current.json",
+        params={"key": API_KEY, "q": "Paris"},
+    )
+    data = response.json()
+
+    location = data["location"]
+    current = data["current"]
+    condition = data["current"]["condition"]
+
+    print(f"Weather in {location['name']}, {location['country']}:")
+    print(f"Temperature: {current['temp_c']}")
+    print(f"Actual condtion: {condition['text']}")
+
+    return data
 
 
 if __name__ == "__main__":
